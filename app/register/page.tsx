8ef5e5d6b545b5bb1a8212b88d099787b827681e
k-ohdate <kento.odate@fox-hound.jp>
2025-05-22 17:43:47 +0900

    task1-3_実装

---
+++ app/register/page.tsx
@@ -0,0 +1,17 @@
+// app/register/page.tsx
+
+"use client"; // クライアントコンポーネントとしてマーク
+
+import React from "react";
+import RegisterForm from "../../components/RegisterForm";
+import { Typography, Box } from "@mui/material";
+
+// TODO: 新規登録ページを実装し、RegisterFormコンポーネントを使用する
+const RegisterPage: React.FC = () => {
+  return (
+    <Box sx={{ maxWidth: 400, mx: "auto", mt: 4 }}>
+      <RegisterForm />
+    </Box>
+  );
+};
+export default RegisterPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React from "react";
+import Link from "next/link";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const Navbar: React.FC = () => {
   return (
@@ -15,9 +15,12 @@ const Navbar: React.FC = () => {
         <Button color="inherit" component={Link} href="/users">
           ユーザー一覧
         </Button>
+        <Button color="inherit" component={Link} href="/register">
+          新規登録
+        </Button>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Navbar;
